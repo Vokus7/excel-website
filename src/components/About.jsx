9825f4f75a400e213a7fc49a4dba59f6ee94a373
Endurance Ogbutor <eddyfree69@gmail.com>
2022-09-17 21:15:51 +0100

    about

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='h-screen w-full'>
+            <div className='max-w-screen-lg px-4 pt-20 mx-auto flex flex-col justify-start w-full h-full'>
+                <div className='mt-2'>
+                    <p className='text-4xl font-bold inline border-b-4 border-gray-500 text-gray-500'>
+                        About
+                    </p>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import HeroImage from "../assets/heroimage.jpg";
 
 const HeroSection = () => {
     return (
-        <div>
-            <div>
-
+        <div className='h-screen w-full'>
+            <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-8 pt-40 md:flex-row">
+                <div className="flex flex-col justify-center py-0 my-0 h-full">
+                    <h2 className="text-5xl sm:text-7xl font-bold text-blue-400">
+                        EXCEL
+                    </h2>
+                    <h4 className="text-3xl sm:text-4xl font-bold text-gray-500 py-2">
+                        PROSTHETICS AND DENTAL CARE
+                    </h4>
+                    <p className="py-4 max-w-md">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum reprehenderit aliquam minima ratione nesciunt! Ipsa dicta dolores commodi nihil consectetur.
+                    </p>
+                </div>
+                <div className="flex flex-col justify-center h-80 md:h-full">
+                    <img src={HeroImage} alt="vector dental clinic" className="mx-10 my-5 md:my-5 w-full self-center h-80 md:h-full md:w-full" />
+                </div>
             </div>
         </div>
     )
