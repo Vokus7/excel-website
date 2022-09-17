5c3655f7b76e3aaa89cb15475cab791e3c640124
Endurance Ogbutor <eddyfree69@gmail.com>
2022-09-17 19:48:07 +0100

    excel_website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import HeroSection from './components/HeroSection';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <NavBar />
+      <HeroSection />
+    </>
   );
 }
 
