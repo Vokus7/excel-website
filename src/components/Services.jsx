43897243b86b69da9de61c9f327f245314b4669b
Endurance Ogbutor <eddyfree69@gmail.com>
2022-09-18 08:13:41 +0100

    excel_website

---
+++ src/components/Services.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Services = () => {
+    return (
+        <div name="about" className='h-screen w-full'>
+            <div className='max-w-screen-lg px-4 pt-20 mx-auto flex flex-col justify-start w-full h-full'>
+                <div className='mt-2'>
+                    <p className='text-4xl font-bold inline border-b-4 border-blue-400 text-blue-400'>
+                        Services
+                    </p>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Services;
