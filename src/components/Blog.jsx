8d0b4adc51ca2399c985987c284402f1b25eb0d3
Endurance Ogbutor <eddyfree69@gmail.com>
2022-10-24 08:15:41 +0100

    excel-website

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import BadBreath from '../assets/Blog/bad-breath.jpg';
@@ -16,7 +17,7 @@ const Blog1 = () => {
                         Blog
                     </p>
                 </div>
-                <Link rel="noopener noreferrer" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 bg-gray-900">
+                <Link rel="noopener noreferrer" className="block max-w-sm gap-3 mx-auto sm:max-w-full lg:grid lg:grid-cols-12 bg-gray-900">
                     <img src={Toothache} alt="toothache" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 bg-gray-500" />
                     <div className="p-6 space-y-2 lg:col-span-5">
                         <h3 className="text-2xl font-semibold sm:text-4xl">WORMS IN THE TEETH</h3>
